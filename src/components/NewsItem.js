c0efad5ad3c45bd0a34bb2c8c32d1cd07a570b87
sanket <sanketthange30@gmail.com>
2024-05-01 22:58:08 +0530

    data is fetched by url

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   
   render() {
-    let {title , description, imageurl,url} = this.props;
+    let {title , description, imageurl, url} = this.props;
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageurl} className="card-img-top" alt="..." />
+          <img src={!imageurl?"https://cdn.ndtv.com/common/images/ogndtv.png":imageurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
